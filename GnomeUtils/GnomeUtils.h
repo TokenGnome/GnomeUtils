d52e814b01fa89032501339c5ba1190a8df1bcc4
Brandon Smith <brandon@tokengnome.com>
2013-06-23 19:59:19 -0700

    Commited multipart body code

---
+++ GnomeUtils/GnomeUtils.h
@@ -0,0 +1,13 @@
+//
+//  GnomeUtils.h
+//  GnomeUtils
+//
+//  Created by Brandon Smith on 6/23/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GnomeUtils : NSObject
+
+@end
