1e582b67eb5cd267588e103e1ed3f249817ea976
Brandon Smith <brandon@tokengnome.com>
2013-06-23 20:57:45 -0700

    Added in application based project.

---
+++ GnomeUtils/AppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.m
+//  GnomeUtils
+//
+//  Created by Brandon Smith on 6/23/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
